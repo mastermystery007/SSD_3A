d0dcf77d2b4c38d162532bc67b180b095b3419ba
Husain Mistry <30475404+mastermystery007@users.noreply.github.com>
2020-11-06 23:06:23 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -25,8 +25,8 @@ class Node:
         
 
 
-leaders1 = []
-leaders2 = []
+leaders = []
+
 def find_leader(id,root):
     
     if(root==None):
@@ -38,10 +38,11 @@ def find_leader(id,root):
     for node in root.nodes:
         addMe = addMe or find_leader(id,node)
         if(addMe):
-            leaders1.append(root.id)  
+            leaders.append(root.id)  
             return True
     
     return False  
+     
 
 
 def get_Level(root,id,level):
@@ -55,27 +56,6 @@ def get_Level(root,id,level):
     for node in root.nodes:
         v=max(v,get_Level(node,id,int(level)+1))
     return v    
-
-
-
-
-
-def find_leader_2(id,root):
-    
-    if(root==None):
-        return False
-
-    if(root.id == id):
-        return True
-    addMe = False
-    for node in root.nodes:
-        addMe = addMe or find_leader_2(id,node)
-        if(addMe):
-            leaders2.append(root.id)  
-            return True
-    
-    return False  
-
 
 
 def add_child(root,node1,val):
@@ -123,10 +103,79 @@ for item in json_decode:
                 add_child(root,temp,json_decode[item][i][keys])
                 pass
             
-employee1=input("Employee1 ")
-employee2=input("Employee2 ")
-find_leader(employee1,root)
-find_leader_2(employee2,root)
+
+
+
+
+
+number = int(input("No of employees: "))
+led = []
+allemployees = []
+
+for j in range (0,number):
+    ledtemp=[]
+    employee=input("Employee name: ")
+    allemployees.append(employee)
+    find_leader(employee,root)
+    #print(leaders)
+    appendlist = []
+    for leader in leaders:
+        #print(leader)
+        appendlist.append(leader)
+    
+    led.append(appendlist)
+    leaders.clear()
+    
+        
+    
+ 
+firstlist = led[0]
+#print(firstlist)
+
+
+finallist = led[0]
+for i in range (0,len(led)):
+    finallist = set(finallist).intersection(led[i])
+    #print(finallist)
+
+
+#print (finallist)
+
+if(len(finallist)==0):
+    print("No common  boss")
+else:
+    leaderfound = False
+    
+    for leader in firstlist:
+        for il in finallist:
+            if(leader == il):
+                leaderfound = True
+                finalleader = leader
+                break
+        if(leaderfound == True):
+            break    
+    print ("common leader is: ",end=" ")
+    print (finalleader)    
+    boss_level = get_Level(root,finalleader,0)
+    #print(boss_level)
+
+    allemployeelevels = []
+    for employee in allemployees:
+        emplevel = get_Level(root,employee,0)
+        allemployeelevels.append(emplevel)    
+    for i in range (0,len(allemployeelevels)):
+        print(str(finalleader)+" is "+str(allemployeelevels[i]-boss_level)+" levels above "+allemployees[i])
+
+
+
+"""
+for leader in firstlist:
+        for interleader in finallist:
+            if(interleaders == leader):
+
+
+
+ return set(lst1).intersection(lst2)
 common_boss_val = 0
 flag=1
 for i in range (0,len(leaders1)):
@@ -150,3 +199,4 @@ emp2_level = get_Level(root,employee2,0)
 if(len(intrsctn)!=0):
     print (str(common_boss_val)+" is "+str(emp1_level-boss_level)+" levels above "+employee1)
     print (str(common_boss_val)+" is "+str(emp2_level-boss_level)+" levels above "+employee2)
+"""
